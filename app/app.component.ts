1819e7feff6944bf7f4578f0b683383c5e877f83
Nam Nguyen <namnguyen@R68-MacMini-01.local>
2016-06-27 11:25:57 -0700

    moved template and styles to own files. linked up different components with routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeroService } from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 
@@ -14,6 +15,7 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/route
   </nav>
   <router-outlet></router-outlet>
   `,
+  styleUrls: ['app/app.component.css'],
   directives: [ROUTER_DIRECTIVES],
   providers: [
     ROUTER_PROVIDERS,
@@ -31,6 +33,11 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/route
     path: '/heroes',
     name: 'Heroes',
     component: HeroesComponent
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
   }
 ])
 export class AppComponent {
--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,7 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from './hero.service';
+import { Router } from '@angular/router-deprecated';
+import { Hero } from './hero'
 
 @Component({
   selector: 'my-dashboard',
-  templateUrl: 'app/dashboard.component.html'
+  templateUrl: 'app/dashboard.component.html',
+  styleUrls: ['app/dashboard.component.css']
 })
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit {
+
+  heroes: Hero[] = [];
+
+  constructor(
+    private router: Router,
+    private heroService: HeroService) {
+
+    }
+
+  ngOnInit() {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1,5));
+  }
+
+  goToDetail(hero: Hero) {
+    let link = ['HeroDetail', {id: hero.id}];
+    this.router.navigate(link);
+  }
+
+}
